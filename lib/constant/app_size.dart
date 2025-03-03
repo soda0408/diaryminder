fdb6194d08db20123cdcf8791f720d7242d95fcd
soda0408 <soooda0408@gmail.com>
2025-03-03 14:37:26 +0900

    app_size.dartファイル追加

---
+++ lib/constant/app_size.dart
@@ -0,0 +1,114 @@
+class FontSize {
+  /// 注意文
+  static const double annotation = 12;
+
+  /// 本文
+  static const double body = 16;
+
+  /// ラベル内
+  static const double label = 18;
+
+  /// 大見出し
+  static const double heading = 20;
+
+  /// 副見出し
+  static const double subheading = 18;
+
+  /// ステータス表示
+  static const double status = 24;
+}
+
+class SpaceSize {
+  /// 基本行間
+  static const double line = 8;
+
+  /// 段落毎の行間
+  static const double paragraph = 16;
+}
+
+class MarginSize {
+  /// 最小のマージン
+  static const double minimum = 8;
+
+  /// 通常のマージン
+  static const double normal = 16;
+
+  /// 水平方向の基本マージン
+  static const double normalVertical = 30;
+
+  /// 垂直方向の基本マージン
+  static const double normalHorizontal = 25;
+
+  /// BottomNavigationBarの高さ
+  static const double kTabBarHeight = 50;
+
+  /// 影のサイズ
+  static const double shadow = 1;
+
+  /// 小さなグリッド用マージン
+  static const double minimumGrid = 2;
+
+  MarginSize(int i);
+}
+
+class PaddingSize {
+  /// 小さめ
+  static const double minimum = 8;
+
+  /// 基本余白
+  static const double normal = 16;
+
+  /// ラベル内の左右余白など
+  static const double content = 32;
+
+  /// コンテンツ用の大きめの余白
+  static const double contentLarge = 40;
+
+  /// ボタンの垂直方向の基本余白
+  static const double buttonVertical = 5;
+
+  // ボタンの垂直方向の大きな余白
+  static const double buttonVerticalLarge = 50;
+
+  /// ボタンの水平方向の基本余白
+  static const double buttonHorizontal = 25;
+
+  /// ボタンの水平方向の大きな余白
+  static const double buttonHorizontalLarge = 30;
+}
+
+class BorderSize {
+  /// 標準の太さ
+  static const double normal = 2;
+
+  /// 太めの太さ
+  static const double bold = 4; // 太めの太さ
+}
+
+class IconSize {
+  /// ヘルプ用
+  static const double help = 20;
+
+  /// ドロワー用
+  static const double drawer = 24;
+
+  /// ボトムバー用
+  static const double navigationItem = 28;
+
+  /// レシピのサムネイル用
+  static const double noImage = 32;
+
+  /// ホームのフードアイコン用
+  static const double homeFoodIcon = 40;
+}
+
+class UiSize {
+  /// カレンダー画面の高さ
+  static const double calendarTileHeight = 180;
+
+  /// 小さいグリッドの角丸の大きさ
+  static const double minimumGridCircular = 8;
+
+  /// Dictionary機能の食品群のBoxの大きさ
+  static const double dictionaryGroupBox = 48;
+}
