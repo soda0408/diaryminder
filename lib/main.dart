4527d8e89952f4c9292854ed60275d233f5e2c0a
Yui_tt <ttymyui@gmail.com>
2025-03-06 10:10:00 +0900

    add modules

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,195 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
+import 'package:cloud_functions/cloud_functions.dart';
 void main() {
-  runApp(const MyApp());
+  // main関数内での非同期処理（下のFirebase.initializeApp）を可能にする処理
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
-
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+// アプリ画面を描画する前に、Firebaseの初期化処理を実行
+class _AppState extends State<App> {
+  Future<FirebaseApp> _initialize() async {
+    return Firebase.initializeApp();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialize(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          // MaterialAppの前なので、 textDirection: TextDirection.ltr
+          // がないと、文字の方向がわからないというエラーになる
+          return Center(
+              child: Text(
+            '読み込みエラー',
+            textDirection: TextDirection.ltr,
+          ));
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+        // 上記と同様。 textDirection: TextDirection.ltr が必要
+        return Center(
+            child: Text(
+          '読み込み中...',
+          textDirection: TextDirection.ltr,
+        ));
+      },
+    );
+  }
+}
+// OCRアプリ画面の描画
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '日本語OCR',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: '日本語OCR'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  // null safety対応のため、Keyに?をつけ、titleは初期値""を設定
+  MyHomePage({Key? key, this.title = ""}) : super(key: key);
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  // null safety対応のため、?でnull許容
+  File? _image;
+  final _picker = ImagePicker();
+  // null safety対応のため、?でnull許容
+  String? _result;
+  @override
+  void initState() {
+    super.initState();
+    _signIn();
+  }
+  // 匿名でのFirebaseログイン処理
+  void _signIn() async {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
+  Future _getImage(FileMode fileMode) async {
+    // null safety対応のため、lateで宣言
+    late final _pickedFile;
+    // image_pickerの機能で、カメラからとギャラリーからの2通りの画像取得（パスの取得）を設定
+    if (fileMode == FileMode.CAMERA) {
+      _pickedFile = await _picker.getImage(source: ImageSource.camera);
+    } else {
+      _pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_pickedFile != null) {
+        _image = File(_pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
     });
   }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('日本語OCR'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          // 写真のサイズによって画面はみ出しエラーが生じるのを防ぐため、
+          // Columnの上にもSingleChildScrollViewをつける
+          child: SingleChildScrollView(
+            child: Column(children: [
+              // 画像を取得できたら表示
+              // null safety対応のため_image!とする（_imageはnullにならない）
+              if (_image != null) Image.file(_image!, height: 400),
+              // 画像を取得できたら解析ボタンを表示
+              if (_image != null) _analysisButton(),
+              Container(
+                  height: 240,
+                  // OCR（テキスト検索）の結果をスクロール表示できるようにするため
+                  // 結果表示部分をSingleChildScrollViewでラップ
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Text((() {
+                        // OCR（テキスト認識）の結果（_result）を取得したら表示
+                        if (_result != null) {
+                          // null safety対応のため_result!とする（_resultはnullにならない）
+                          return _result!;
+                        } else if (_image != null) {
+                          return 'ボタンを押すと解析が始まります';
+                        } else {
+                          return 'OCR（テキスト認識）したい画像を撮影または読込んでください';
+                        }
+                      }())))),
+            ]),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // カメラ起動ボタン
+          FloatingActionButton(
+            onPressed: () => _getImage(FileMode.CAMERA),
+            tooltip: 'Pick Image from camera',
+            child: Icon(Icons.camera_alt),
+          ),
+          // ギャラリー（ファイル）検索起動ボタン
+          FloatingActionButton(
+            onPressed: () => _getImage(FileMode.GALLERY),
+            tooltip: 'Pick Image from gallery',
+            child: Icon(Icons.folder_open),
+          ),
+        ],
+      ),
     );
   }
+  // OCR（テキスト認識）開始処理
+  Widget _analysisButton() {
+    return ElevatedButton(
+      child: Text('解析'),
+      onPressed: () async {
+        // null safety対応のため_image!とする（_imageはnullにならない）
+        List<int> _imageBytes = _image!.readAsBytesSync();
+        String _base64Image = base64Encode(_imageBytes);
+        // Firebase上にデプロイしたFunctionを呼び出す処理
+        HttpsCallable _callable =
+            FirebaseFunctions.instance.httpsCallable('annotateImage');
+        final params = '''{
+          "image": {"content": "$_base64Image"},
+          "features": [{"type": "TEXT_DETECTION"}],
+          "imageContext": {
+            "languageHints": ["ja"]
+          }
+        }''';
+        final _text = await _callable(params).then((v) {
+          return v.data[0]["fullTextAnnotation"]["text"];
+        }).catchError((e) {
+          print('ERROR: $e');
+          return '読み取りエラーです';
+        });
+        // OCR（テキスト認識）の結果を更新
+        setState(() {
+          _result = _text;
+        });
+      },
+    );
+  }
+}
+// カメラ経由かギャラリー（ファイル）経由かを示すフラグ
+enum FileMode{
+  CAMERA,
+  GALLERY,
 }
